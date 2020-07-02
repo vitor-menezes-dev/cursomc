945350d6d2fef51b147ecf1f1922101291e9abd8
Sousa <vitor.menezes@capgemini.com>
2020-07-02 15:25:21 -0300

    Validação customizada: email não repetido na inserção de Cliente

--- src/main/java/com/teste/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/teste/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,17 @@ package com.teste.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.teste.cursomc.domain.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	@Transactional
+	Cliente findByEmail(String email);
+	
+	@Transactional
+	Cliente findByCpfOuCnpj(String cpfOuCnpj);
+	
 }
--- src/main/java/com/teste/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/teste/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.teste.cursomc.domain.Cliente;
 import com.teste.cursomc.domain.enums.TipoCliente;
 import com.teste.cursomc.dto.ClienteNewDTO;
+import com.teste.cursomc.repositories.ClienteRepository;
 import com.teste.cursomc.resources.exceptions.FieldMessage;
 import com.teste.cursomc.services.validation.util.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	
+	@Autowired
+	ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -33,6 +41,15 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 			list.add(new FieldMessage("cpfOuCnpj","CNPJ Inválido"));
 		}
 		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux !=null) {
+			list.add(new FieldMessage("email","Email já cadastrado"));
+		}
+		
+		aux = repo.findByCpfOuCnpj(objDto.getCpfOuCnpj());
+		if(aux !=null) {
+			list.add(new FieldMessage("cpfOuCnpj","CPF ou CNPJ já cadastrado"));
+		}
 		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
