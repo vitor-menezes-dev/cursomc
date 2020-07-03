63f07a0d83c3f889ac7b3b44176dcaf65eb20a91
Sousa <vitor.menezes@capgemini.com>
2020-07-03 11:26:30 -0300

    Criando o profile de desenvolvimento

---
+++ src/main/java/com/teste/cursomc/config/DevConfig.java
@@ -0,0 +1,30 @@
+package com.teste.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.teste.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Value("$(spring.jpa.hibernate.ddl-auto)")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		if (strategy.contains("create"))
+			dbService.instantieteTestDatabase();
+		return true;
+	}
+
+}
