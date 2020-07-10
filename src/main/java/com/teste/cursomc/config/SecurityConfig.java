d8565ec12768958481a264f4c7ef2210143c8faa
Sousa <vitor.menezes@capgemini.com>
2020-07-10 15:45:11 -0300

    Implementando autorização

--- src/main/java/com/teste/cursomc/config/SecurityConfig.java
+++ src/main/java/com/teste/cursomc/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.teste.cursomc.security.JWTAuthenticationFilter;
+import com.teste.cursomc.security.JWTAuthorizationFilter;
 import com.teste.cursomc.security.JWTUtil;
 
 @Configuration
@@ -52,6 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		.antMatchers(PUBLIC_MATCHERS).permitAll()
 		.anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil,userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
