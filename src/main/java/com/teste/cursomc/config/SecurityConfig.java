2cbbbf65636f4251b0357177cf306229411db0ce
Sousa <vitor.menezes@capgemini.com>
2020-07-13 11:10:26 -0300

    Autorizando endpoints para perfis específicos

--- src/main/java/com/teste/cursomc/config/SecurityConfig.java
+++ src/main/java/com/teste/cursomc/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -24,36 +25,38 @@ import com.teste.cursomc.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserDetailsService userDetailsService;
-	
+
 	@Autowired
 	private Environment env;
 
 	@Autowired
 	private JWTUtil jwtUtil;
-	
-	private static String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**","/clientes/**", };
-	
+
+	private static String[] PUBLIC_MATCHERS_POST = { "/clientes/**", };
+
+	private static String[] PUBLIC_MATCHERS_GET = { "/produtos/**", "/categorias/**" };
+
 	private static String[] PUBLIC_MATCHERS = { "/h2-console/**" };
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		
-		//Condição feita para liberar acesso ao console do h2
-		if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+
+		// Condição feita para liberar acesso ao console do h2
+		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
-		
+
 		http.cors().and().csrf().disable();
-		http.authorizeRequests()
-		.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
-		.antMatchers(PUBLIC_MATCHERS).permitAll()
-		.anyRequest().authenticated();
+		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_POST).permitAll()
+				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
-		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil,userDetailsService));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
@@ -61,19 +64,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(AuthenticationManagerBuilder auth) throws Exception {
 		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
 	}
-	
+
 	@Bean
 	CorsConfigurationSource corsConfigurationSource() {
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
 		return source;
 	}
-	
+
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {
-		return new BCryptPasswordEncoder(); 
+		return new BCryptPasswordEncoder();
 	}
-	
-	
 
 }
