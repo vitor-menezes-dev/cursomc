1d2e81159e58ee021a001738b341d318440d9e45
Sousa <vitor.menezes@capgemini.com>
2020-07-14 16:11:37 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/teste/cursomc/CursomcApplication.java
+++ src/main/java/com/teste/cursomc/CursomcApplication.java
@@ -1,13 +1,17 @@
 package com.teste.cursomc;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.teste.cursomc.services.S3Service;
+
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-
+	@Autowired
+	S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -16,7 +20,8 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		
-
+		s3Service.uploadFile("c:\\Ambiente\\neblina.jpg","neblina.jpg");
+		s3Service.uploadFile("c:\\Ambiente\\neblina.mp4","neblina.mp4");
 	}
 
 }
